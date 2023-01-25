fe2845ac577fa91e6fca3dee243552c04dbfd701
KerrMadeleine <102504165+KerrMadeleine@users.noreply.github.com>
2023-01-25 10:54:55 -0800

    added the second PCA fnct
    this one makes a 10x11 plot where one axis is the integer and the other is the p used for PCA, adding the OG image at the end of each row.

--- image.py
+++ image.py
@@ -68,3 +68,29 @@ def make_PCA_figure_1(trainBool,img_idx,pvals,errBool):
     plt.show()
     plt.clf()
     plt.close()
+    
+    def make_PCA_figure_2(trainBool,errBool):
+        fig, ax = plt.subplots(10,11)
+        fig.set_size_inches(7,7)
+        X, y = data.load_data(os.path.join(os.path.dirname(__file__), 'data'), train=trainBool)
+        #print(np.argwhere(y==2).flatten())
+        idx_list=[np.random.choice(np.argwhere(y==ii).flatten()) for ii in range(10)]
+        ax[0][10].set_title("original")
+        for p in np.arange(10): # integer class
+            pca = PCA(n_components=p)#hyperparameters.p)
+            pca.fit(X)
+            transformedTestData=pca.transform(X)
+            retransformedData=pca.inverse_transform(transformedTestData)
+            print(np.shape(retransformedData))
+            ax[0][p].set_title('p='+str(p))
+            for i in np.arange(10): # p value
+                idx=idx_list[i]
+                ax[i][p].pcolor(retransformedData[idx].reshape(28,28),cmap='gray',vmin=0,vmax=200)
+                ax[i][p].set_axis_off()
+                ax[i][p].set_ylim(ax[i][p].get_ylim()[::-1])
+                ax[i][10].pcolor(X[idx].reshape(28,28),cmap='gray',vmin=0,vmax=200)
+                ax[i][10].set_axis_off()
+                ax[i][10].set_ylim([28,0])
+        plt.show()
+        plt.clf()
+        plt.close()
