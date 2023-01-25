3f815edca74056fb5532955b8db445d167bab541
KerrMadeleine <102504165+KerrMadeleine@users.noreply.github.com>
2023-01-25 09:46:16 -0800

    Update image.py
    add the function which makes the first PCA image.

--- image.py
+++ image.py
@@ -31,3 +31,38 @@ def create_loss_plot(train_loss, val_loss, name='average_loss.png'):
     plt.ylabel('Loss')
     plt.legend()
     plt.savefig(os.path.join(os.path.dirname(__file__), 'images', name))
+    
+def make_PCA_figure_1(trainBool,img_idx,pvals,errBool):
+    """
+    Inputs:
+    trainBool (Boolean): use the training data (True) or the test data (False)
+    img_idx (int): the random index of the image in the dataset to use
+    pvals (list): a list of the 7 p values to apply to the image
+    errBool (Boolean): True if you want the absolute error of reconstructed image with original image
+    
+    pyplot shows a 4x2 image of the 7 reconstructed images plus the original one.
+    """
+    fig, ax = plt.subplots(4,2)
+    fig.set_size_inches(5,7)
+    fig.suptitle('Original and reconstructed images')
+    X_test, y_test = data.load_data(os.path.join(os.path.dirname(__file__), 'data'), train=trainBool)
+    pltcnt=0
+    for p in pvals:
+        pca = PCA(n_components=p)#hyperparameters.p)
+        pca.fit(X_test)
+        transformedTestData=pca.transform(X_test)
+        retransformedData=pca.inverse_transform(transformedTestData)
+        ax[pltcnt//2][pltcnt%2].set_title('p='+str(p))
+        if errBool:
+            ax[pltcnt//2][pltcnt%2].pcolor(np.abs(retransformedData[img_idx].reshape(28,28)-X_test[img_idx].reshape(28,28)),cmap='gray',vmin=0,vmax=200)
+        else:
+            ax[pltcnt//2][pltcnt%2].pcolor(retransformedData[img_idx].reshape(28,28),cmap='gray',vmin=0,vmax=200)
+        ax[pltcnt//2][pltcnt%2].set_axis_off()
+        pltcnt+=1
+    ax[3][1].pcolor(X_test[img_idx].reshape(28,28),cmap='gray',vmin=0,vmax=200)
+    ax[3][1].set_title("original image")
+    ax[3][1].set_axis_off()
+    #plt.savefig('BLthickness_'+MODS[i][:-1]+'.png',dpi=300)
+    plt.show()
+    plt.clf()
+    plt.close()
